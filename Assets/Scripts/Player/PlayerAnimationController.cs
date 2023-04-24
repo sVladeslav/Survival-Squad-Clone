58f4c5efdae98a9938e0407869cbae0986c164be
Vladyslav Svistielnik <svistv96@gmail.com>
2023-04-24 09:27:56 +0300

    create main scene; add solder player; add animation of idle, running, attack.convert project to URP

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        [Tooltip("Animator component from child model")]
+        [SerializeField] private Animator _animator;
+
+        private string _topBodyLayer = "Top body";
+        
+        private int _attackLayerIndex;
+        
+        private string _isRunningParameter = "isRunning";
+        private string _isAttackingParameter = "isAttacking";
+
+        private void Awake()
+        {
+            _attackLayerIndex = _animator.GetLayerIndex(_topBodyLayer);
+        }
+
+        public void PlayMovementAnimation(bool isRunning)
+        {
+            _animator.SetBool(_isRunningParameter, isRunning);
+        }
+        
+        public void PlayAttackAnimation(bool isAttacking)
+        {
+            _animator.SetBool(_isAttackingParameter, isAttacking);
+            
+            _animator.SetLayerWeight(_attackLayerIndex, WeightFromBool(isAttacking));
+        }
+
+        private int WeightFromBool(bool state)
+        {
+            if (state)
+                return 1;
+            return 0;
+        }
+    }
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private MoveByPhysicsJoysticController _playerMovement;
+
+        [SerializeField] private PlayerAnimationController _animationController;
+        
+      
+        public MoveByPhysicsJoysticController PlayerMovement => _playerMovement;
+        
+        private void OnEnable()
+        {
+            _playerMovement.OnPlayerMove += _animationController.PlayMovementAnimation;
+        }
+
+        private void OnDisable()
+        {
+            _playerMovement.OnPlayerMove -= _animationController.PlayMovementAnimation;
+        }
+    }
