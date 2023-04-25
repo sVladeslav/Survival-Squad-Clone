6d5230ab2734bcaac03d1d7897a3d8a39b3ac227
Vladyslav Svistielnik <svistv96@gmail.com>
2023-04-25 17:03:37 +0300

    health system

---
+++ Assets/Scripts/Player/HealthSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UnityTemplateProjects.Player
+{
+    public class HealthSystem
+    {
+        public event Action OnHealthChanged;
+
+        private int _maxHealth;
+        private int _health;
+
+        public HealthSystem(int maxHealth)
+        {
+            _maxHealth = maxHealth;
+            _health = maxHealth;
+        }
+
+        public float GetHealthPercent()
+        {
+            return (float) _health / _maxHealth;
+        }
+        
+        public void Damage(int damageValue)
+        {
+            _health -= damageValue;
+            if (_health < 0)
+                _health = 0;
+            
+            OnHealthChanged?.Invoke();
+        }
+
+        public void Heal(int healValue)
+        {
+            _health += healValue;
+            if (_health > _maxHealth)
+                _health = _maxHealth;
+            
+            OnHealthChanged?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/View/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityTemplateProjects.Player;
+
+namespace UnityTemplateProjects.View
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Transform pfHealthBar;
+        
+        private HealthSystem _heathSystem;
+
+        public void Setup(HealthSystem healthSystem)
+        {
+            _heathSystem = healthSystem;
+
+            _heathSystem.OnHealthChanged += HeathSystem_OnChangeHealth;
+        }
+
+        private void HeathSystem_OnChangeHealth()
+        {
+            pfHealthBar.localScale = new Vector3(_heathSystem.GetHealthPercent(), 1);
+        }
+    }
+}
