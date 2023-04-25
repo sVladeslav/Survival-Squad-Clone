e59527932f81b722de5d8d9da9206cf2e69ad0f5
Vladyslav Svistielnik <svistv96@gmail.com>
2023-04-26 00:30:01 +0300

    WeaponManager

---
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace UnityTemplateProjects.Player
+{
+    public class WeaponManager: MonoBehaviour
+    {
+        [SerializeField] private MultiParentConstraint ParentConstraint;
+
+        public void ChangeStateWeapon()
+        {
+            ParentConstraint.weight = ParentConstraint.weight == 1
+                ? 0
+                : 1;
+        }
+    }
+}
