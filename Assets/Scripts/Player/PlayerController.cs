31255aaedabf6d18b3e798497203e519c46108c6
Vladyslav Svistielnik <svistv96@gmail.com>
2023-04-25 22:50:56 +0300

    assign health system to health bar

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,9 @@
 ﻿
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityTemplateProjects.Player;
+using UnityTemplateProjects.View;
 
 public class PlayerController : MonoBehaviour
     {
@@ -9,6 +12,10 @@ public class PlayerController : MonoBehaviour
         [SerializeField] private PlayerAnimationController _animationController;
         
         [SerializeField] private PlayerAttackController _attackController;
+
+        [SerializeField] private HealthBar _healthBarPrefab;
+
+        private HealthSystem _healthSystem;
         
       
         public PlayerMoveController PlayerMovement => _playerMovement;
@@ -17,6 +24,12 @@ public class PlayerController : MonoBehaviour
         {
             _playerMovement.OnPlayerMove += _animationController.PlayMovementAnimation;
             _attackController.OnAttack += _animationController.PlayAttackAnimation;
+
+            var healthObject = Instantiate(_healthBarPrefab, new Vector3(0, 2.7f, 0), quaternion.identity, transform);
+            var healthBar = healthObject.GetComponent<HealthBar>();
+
+            _healthSystem = new HealthSystem(100);
+            healthBar.Setup(_healthSystem);
         }
 
         private void OnDisable()
