71d5f7d17e49429dd400ac24096f105ff11fa5cc
Vladyslav Svistielnik <svistv96@gmail.com>
2023-04-30 16:47:59 +0300

    add navmesh agent for enemy

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityTemplateProjects.General;
 using UnityTemplateProjects.Player;
 using UnityTemplateProjects.View;
@@ -12,14 +13,20 @@ namespace UnityTemplateProjects.Enemy
         [SerializeField] private EnemyAttackController _attackController;
         [SerializeField] private EnemyAnimationController _animationController;
         
+        [SerializeField] private Transform _playerTarget;
+
         [SerializeField] private HealthBar _healthBar;
 
+        private NavMeshAgent _navMeshAgent;
+        
         private HealthSystem _healthSystem;
 
         private void Start()
         {
             _healthSystem = new HealthSystem(50);
             _healthBar.Setup(_healthSystem);
+
+            _navMeshAgent = GetComponent<NavMeshAgent>();
         }
 
         public void ReceiveDamage(float damageValue)
@@ -28,6 +35,11 @@ namespace UnityTemplateProjects.Enemy
             
             if(_healthSystem.GetHealthPercent() == 0)
                 Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            _navMeshAgent.SetDestination(_playerTarget.position);
         }
     }
 }
